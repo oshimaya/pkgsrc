f38937d5f8d3146bbdcbf47ce1bde79b75646d78
Yasushi Oshima <oshima-ya@yagoto-urayama.jp>
2024-06-30 16:13:21 +0900

    db4-4.8.30nb1 master(2024/6/18)

--- databases/db4/options.mk
+++ databases/db4/options.mk
@@ -2,16 +2,10 @@
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.db4
 
-PKG_SUPPORTED_OPTIONS=	doc posixmutexes tcl
+PKG_SUPPORTED_OPTIONS=	doc
 PKG_SUGGESTED_OPTIONS=	doc
 
 .include "../../mk/bsd.prefs.mk"
-
-.include "../../mk/bsd.prefs.mk"
-.if (${MACHINE_ARCH} == "sh3el" || ${MACHINE_ARCH} == "sh3eb" || \
-     ${MACHINE_ARCH} == "vax" ) && (${OPSYS} == "NetBSD")
-PKG_SUGGESTED_OPTIONS+=        posixmutexes
-.endif
 
 .if ${OPSYS} == "MirBSD"
 PLIST_SRC+=		PLIST.${OPSYS}
@@ -31,13 +25,4 @@ SUBST_FILES.docs=	dist/Makefile.in
 SUBST_SED.docs=		-e '/^library_install:/s, install_docs,,'
 .else
 PLIST_SRC+=		PLIST.docs
-.endif
-
-.if !empty(PKG_OPTIONS:Mposixmutexes)
-CONFIGURE_ARGS+=       --enable-posixmutexes
-.endif
-
-.if !empty(PKG_OPTIONS:Mtcl)
-.include "../../lang/tcl85/buildlink3.mk"
-CONFIGURE_ARGS+=       --with-tcl=${BUILDLINK_PREFIX.tcl}/lib
 .endif
