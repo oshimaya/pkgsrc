e1d8cab7d349a1519e2e1869b6a1a1e80712f5e5
Yasushi Oshima <oshima-ya@yagoto-urayama.jp>
2024-06-30 16:16:30 +0900

    base : db4-4.8.30nb1(2024/6/18) : repatch add db4 pkg which supports posixmutex option to be able to build on NetBSD/sh3 and vax

--- databases/db4/options.mk
+++ databases/db4/options.mk
@@ -2,10 +2,18 @@
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.db4
 
-PKG_SUPPORTED_OPTIONS=	doc
+PKG_SUPPORTED_OPTIONS=	doc posixmutexes
 PKG_SUGGESTED_OPTIONS=	doc
 
 .include "../../mk/bsd.prefs.mk"
+
+### db4 has no MUTEX GCC ASSEMBLY for NetBSD/sh3 and vax
+###  but posixmutexes option is useful for them
+###
+.if (${MACHINE_ARCH} == "sh3el" || ${MACHINE_ARCH} == "sh3eb" || \
+     ${MACHINE_ARCH} == "vax" ) && (${OPSYS} == "NetBSD")
+PKG_SUGGESTED_OPTIONS+= posixmutexes
+.endif
 
 .if ${OPSYS} == "MirBSD"
 PLIST_SRC+=		PLIST.${OPSYS}
@@ -16,6 +24,7 @@ PLIST_SRC+=		PLIST
 .include "../../mk/bsd.options.mk"
 
 ###
+###
 ### Install documentation files
 ###
 .if empty(PKG_OPTIONS:Mdoc)
@@ -25,4 +34,12 @@ SUBST_FILES.docs=	dist/Makefile.in
 SUBST_SED.docs=		-e '/^library_install:/s, install_docs,,'
 .else
 PLIST_SRC+=		PLIST.docs
+.endif
+
+###
+### Force use of POSIX standard mutexes
+###  for some architectures which has no MUTEX GCC ASSEMBLY
+###
+.if !empty(PKG_OPTIONS:Mposixmutexes)
+CONFIGURE_ARGS+=       --enable-posixmutexes
 .endif
